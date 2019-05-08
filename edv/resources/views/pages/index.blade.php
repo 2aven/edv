78f8c300a74bda22ddf8f9f1df26bea3ddfd819a
Andreu Bió <abio@iesfbmoll.org>
2019-05-08 22:40:13 +0200

    lang ca,es,en; PagesController

---
+++ edv/resources/views/pages/index.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config('app.name','eDv')}}</title>
+
+  </head>
+  <body>
+
+
+    <div >
+        <div >
+            <h1> Entrenador Dvorak </h1>
+            <p>Si així no n'aprens, és que no tens remei</p>
+        </div>
+
+        <div class="links">
+            <a href="#">Link</a>
+        </div>
+    </div>
+
+  </body>
+</html>
